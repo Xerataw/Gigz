eb083b92485f61a528aca6d10af58e36e08b0134
alexis-moins <sinus.nacre0v@icloud.com>
2023-06-13 19:51:28 +0200

    fix(api): fixed autoreload in docker [wip]

--- GIGZ-BACK/src/server.ts
+++ GIGZ-BACK/src/server.ts
@@ -1,15 +1,18 @@
 import express from "express";
 
 import v1 from "./v1";
+import useDatabase from "./composition/useDatabase";
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+const { database } = useDatabase()
+
 app.use(express.json());
 
-app.use(function (req, _, next) {
-  console.log("🚥 %s %s", req.method, req.path);
-  next();
+app.use(function(req, _, next) {
+	console.log("🚥 %s %s", req.method, req.path);
+	next();
 });
 
 app.use("/api/v1/", v1);
--- GIGZ-BACK/src/v1/index.ts
+++ GIGZ-BACK/src/v1/index.ts
@@ -1,12 +1,16 @@
 import express from "express";
-
+import useDatabase from "../composition/useDatabase";
 
 const router = express.Router();
+const { database } = useDatabase()
 
-router.get("/status", (_, res) => {
+router.get("/status", async (_, res) => {
+	const a = await database.account.findMany({})
+
 	res.status(200).json({
 		status: 200,
-		message: "Welcome to the api!"
+		message: "Welcome to the api!",
+		data: a,
 	})
 });
 
