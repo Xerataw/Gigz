c3e8f99fab35209d79d906479121fe19de9abc29
Fil Veith <fil.veith@epitech.eu>
2023-06-19 11:38:12 +0200

    Init alexis

---
+++ gigz/src/views/Tab1Page.vue
@@ -0,0 +1,27 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Tab 1</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+			<Tab4Page />
+
+      <ExploreContainer name="Tab 1 page" />
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import ExploreContainer from '@/components/ExploreContainer.vue';
+
+import Tab4Page from '@/components/Tab4Page.vue';
+</script>
