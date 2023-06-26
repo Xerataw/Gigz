c44dd3c0cdf8469711d471c415ebb66c73c8fe40
alexis-moins <sinus.nacre0v@icloud.com>
2023-06-26 13:14:10 +0200

    feat(auth): added first iteration of login route [wip]

--- back/src/auth/login.ts
+++ back/src/auth/login.ts
@@ -1,11 +1,40 @@
 import express from 'express';
+import { z } from 'zod';
+
+import useHash from '../composables/useHash';
+import useToken from '../composables/useToken';
+import useDatabase from '../composables/useDatabase';
 
 const router = express.Router();
 
-router.get('/', async (_, res) => {
+const { compare } = useHash();
+const { generateToken } = useToken();
+const { findAccountByEmail } = useDatabase();
+
+router.post('/', async (req, res) => {
+  const BodySchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+  });
+
+  const body = BodySchema.safeParse(req.body);
+  if (!body.success) return; // TODO
+
+  const account = await findAccountByEmail(body.data.email);
+  if (!account) return; // TODO
+
+  if (!(await compare(body.data.password, account.password))) return; // TODO
+
+  const token = generateToken({
+    id: account.id,
+    email: account.email,
+  });
+
   res.status(200).json({
     success: true,
-    message: 'Welcome to the gigz api!',
+    data: {
+      token: token,
+    },
   });
 });
 
