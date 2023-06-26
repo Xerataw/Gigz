7d8b56c25f50d536ff93a00fb2da3b8cf70f1e23
alexis-moins <sinus.nacre0v@icloud.com>
2023-06-26 14:21:15 +0200

    feat(useUtils): added sendError and sendResponse functions

--- back/src/composables/useUtils.ts
+++ back/src/composables/useUtils.ts
@@ -1,15 +1,21 @@
+import { Response } from 'express';
+
 /**
  * Enumeration containing all API messages return to the front-end.
  */
 enum ApiMessages {
   MissingToken = 'MISSING_TOKEN',
   WrongToken = 'WRONG_TOKEN',
+  WrongPassword = 'WRONG_PASSWORD',
+
+  BadRequest = 'BAD_REQUEST',
 
   ApiRunning = 'API_RUNNING',
 }
 
 /**
- * Return the specified environment varibale or throw an error if it is not set.
+ * Return the specified environment varibale or throw an error if it is not
+ * set.
  */
 const getEnv = (name: string) => {
   const variable = process.env[name];
@@ -20,10 +26,38 @@ const getEnv = (name: string) => {
   return variable;
 };
 
+/**
+ * Send a formatted error response to the client with the given error message
+ * and a specific status code (default: 400 BAD REQUEST).
+ */
+const sendError = (
+  response: Response,
+  message: ApiMessages,
+  statusCode = 400
+) => {
+  response.status(statusCode).json({
+    success: false,
+    message: message,
+  });
+};
+
+/**
+ * Send a formatted success response to the client with the given data  and a
+ * specific status code (default: 200 OK).
+ */
+const sendResponse = (response: Response, data: any, statusCode = 200) => {
+  response.status(statusCode).json({
+    success: true,
+    data: data,
+  });
+};
+
 const useUtils = () => ({
   ApiMessages,
 
   getEnv,
+  sendError,
+  sendResponse,
 });
 
 export default useUtils;
