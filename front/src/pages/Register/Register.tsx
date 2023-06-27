31cf79201321c0b9b02d3d3245696c7a829a6d85
iittooss <brgtamaury@gmail.com>
2023-06-27 14:50:09 +0200

    fix(form): move into multiples files the account form

--- front/src/pages/Register/Register.tsx
+++ front/src/pages/Register/Register.tsx
@@ -34,17 +34,7 @@
   ---> Send form to complete profile
 */
 
-import {
-  Button,
-  Group,
-  Loader,
-  PasswordInput,
-  Stepper,
-  Text,
-  TextInput,
-  Title,
-  Tooltip,
-} from '@mantine/core';
+import { Button, Group, Loader, Stepper, Text } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useDebouncedValue } from '@mantine/hooks';
 import {
@@ -54,15 +44,11 @@ import {
   IconInfoCircle,
   IconShieldLock,
 } from '@tabler/icons-react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import { v4 } from 'uuid';
-
-interface UserTypeButton {
-  labelTooltip: string;
-  label: string;
-  value: 'isHost' | 'isArtist';
-}
+import FirstStep from './FirstStep';
+import SecondStep from './SecondStep';
+import ThirdStep from './ThirdStep';
 
 const errorPassword = (value: string) => (
   <div>
@@ -75,19 +61,6 @@ const errorPassword = (value: string) => (
     </ul>
   </div>
 );
-
-const userTypes: UserTypeButton[] = [
-  {
-    labelTooltip: 'Un artiste, un groupe',
-    label: 'Un artiste',
-    value: 'isArtist',
-  },
-  {
-    labelTooltip: 'Une salle, un bar, un resto',
-    label: 'Un host',
-    value: 'isHost',
-  },
-];
 
 const artistPath = '/login/register/artist';
 const hostPath = '/login/register/host';
@@ -104,34 +77,33 @@ const Register: React.FC = () => {
       userType: '',
     },
     validate: (values) => {
-      if (formStep === 0) {
-        return {
-          userType: values.userType !== '' ? '' : 'Veuillez choisir un type',
-        };
-      }
+      switch (formStep) {
+        case 0:
+          return {};
 
-      if (formStep === 1) {
-        return {
-          email: /^\S+@\S+$/.test(values.email) ? null : 'Email Invalide',
-          phone: /^.{10}$/.test(values.phone) ? null : 'Numéro invalide',
-        };
-      }
+        case 1:
+          return {
+            email: /^\S+@\S+$/.test(values.email) ? null : 'Email Invalide',
+            phone: /^.{10}$/.test(values.phone) ? null : 'Numéro invalide',
+          };
 
-      if (formStep === 2) {
-        return {
-          password: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(
-            values.password
-          )
-            ? null
-            : errorPassword(values.password),
-          confirmPassword:
-            values.confirmPassword === values.password
-              ? null
-              : 'Les mots de passe ne correspondent pas',
-        };
-      }
+        case 2:
+          return {
+            password:
+              /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(
+                values.password
+              )
+                ? null
+                : errorPassword(values.password),
+            confirmPassword:
+              values.confirmPassword === values.password
+                ? null
+                : 'Les mots de passe ne correspondent pas',
+          };
 
-      return {};
+        default:
+          return {};
+      }
     },
   });
 
@@ -186,64 +158,13 @@ const Register: React.FC = () => {
     <div className="pt-10 border border-red-500 flex flex-col items-center">
       <Stepper active={formStep} orientation="horizontal" p="xl" w={'100%'}>
         <Stepper.Step icon={<IconDisc />}>
-          <Title>Vous êtes </Title>
-          <Button.Group orientation="vertical">
-            {userTypes.map((item) => (
-              <Tooltip
-                key={v4()}
-                label={item.labelTooltip}
-                withinPortal
-                onClick={() => {
-                  form.setValues((values) => ({
-                    ...values,
-                    userType: item.value,
-                  }));
-                  setFormStep((old) => old + 1);
-                }}
-              >
-                <Button
-                  size="xl"
-                  m="lg"
-                  variant={
-                    form.values.userType === item.value ? 'filled' : 'outline'
-                  }
-                >
-                  {item.label}
-                </Button>
-              </Tooltip>
-            ))}
-          </Button.Group>
+          <FirstStep form={form} nextStep={() => nextStep()} />
         </Stepper.Step>
-
         <Stepper.Step icon={<IconInfoCircle />}>
-          <Title>Pour vous retrouver</Title>
-          <TextInput
-            autoFocus
-            mt="sm"
-            label="Email"
-            placeholder="email@host.com"
-            {...form.getInputProps('email')}
-          />
-          <TextInput
-            mt="sm"
-            label="Téléphone"
-            placeholder="0695818549"
-            {...form.getInputProps('phone')}
-          />
+          <SecondStep form={form} nextStep={() => nextStep()} />
         </Stepper.Step>
-
         <Stepper.Step icon={<IconShieldLock />}>
-          <Title>La sécurité</Title>
-          <PasswordInput
-            autoFocus
-            label="Mot de passe"
-            {...form.getInputProps('password')}
-          />
-          <PasswordInput
-            mt="sm"
-            label="Répéter le mot de passe"
-            {...form.getInputProps('confirmPassword')}
-          />
+          <ThirdStep form={form} nextStep={() => nextStep()} />
         </Stepper.Step>
 
         <Stepper.Step
@@ -271,9 +192,7 @@ const Register: React.FC = () => {
           </Button>
         )}
         {formStep > 0 && formStep < 3 && (
-          <Button disabled={false} onClick={nextStep}>
-            Prochaine étape
-          </Button>
+          <Button onClick={nextStep}>Prochaine étape</Button>
         )}
       </Group>
       <div className="text-center mt-10">
