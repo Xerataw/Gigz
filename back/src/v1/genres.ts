343a9e13be1fb96d9b42e66aaa9b54d47fe3d0a3
Oxy <lucas.nouhaud@gmail.com>
2023-07-04 12:59:10 +0200

    feat: add the get genres route

---
+++ back/src/v1/genres.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+import useDatabase from '@composables/useDatabase';
+import useUtils from '@composables/useUtils';
+
+const router = express.Router();
+const { database } = useDatabase();
+const { sendResponse, fromDbFormat } = useUtils();
+
+router.get('/', async (req, res) => {
+  const data = await database.genre.findMany({});
+
+  sendResponse(res, fromDbFormat(data), 200);
+});
+
+export default router;
--- back/src/v1/index.ts
+++ back/src/v1/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import profile from '@v1/profile';
 import accounts from '@v1/accounts';
+import genres from '@v1/genres';
 
 const router = express.Router();
 
 router.use('/profiles', profile);
 router.use('/accounts', accounts);
+router.use('/genres', genres);
 
 export default router;
