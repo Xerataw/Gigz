ac1ab85a88ed2a12d7fefe329056491376d02439
Oxy <lucas.nouhaud@gmail.com>
2023-07-04 13:06:57 +0200

    feat: add the get capacities route

---
+++ back/src/v1/capacities.ts
@@ -0,0 +1,16 @@
+import useDatabase from '@composables/useDatabase';
+import useUtils from '@composables/useUtils';
+import express from 'express';
+
+const { database } = useDatabase();
+const { fromDbFormat, sendResponse } = useUtils();
+
+const router = express.Router();
+
+router.get('/', async (_, res) => {
+  const data = await database.capacity.findMany();
+
+  sendResponse(res, fromDbFormat(data), 200);
+});
+
+export default router;
--- back/src/v1/index.ts
+++ back/src/v1/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import profile from '@v1/profile';
 import accounts from '@v1/accounts';
+import capacities from '@v1/capacities';
 
 const router = express.Router();
 
 router.use('/profiles', profile);
 router.use('/accounts', accounts);
+router.use('/capacities', capacities);
 
 export default router;
