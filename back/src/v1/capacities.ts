df6a0fee1d8f5337400fa069727c042063a2fa9d
Oxy <lucas.nouhaud@gmail.com>
2023-07-04 14:32:09 +0200

    feat: delete the useless error handling, look to the YEP-172

--- back/src/v1/capacities.ts
+++ back/src/v1/capacities.ts
@@ -3,18 +3,14 @@ import useUtils from '@composables/useUtils';
 import express from 'express';
 
 const { database } = useDatabase();
-const { ApiMessages, sendError, sendResponse, fromDbFormat } = useUtils();
+const { sendResponse, fromDbFormat } = useUtils();
 
 const router = express.Router();
 
 router.get('/', async (_, res) => {
-  try {
-    const data = await database.capacity.findMany();
+  const data = await database.capacity.findMany();
 
-    sendResponse(res, fromDbFormat(data), 200);
-  } catch (e) {
-    sendError(res, ApiMessages.SERVER_ERROR, 500);
-  }
+  sendResponse(res, fromDbFormat(data), 200);
 });
 
 export default router;
