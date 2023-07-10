d8a724b913499e7f5291cf518cb15dfd5994e1e7
Fil Veith <fil.veith@epitech.eu>
2023-07-10 16:46:59 +0200

    feat(chat list): display a chat list

--- front/src/components/ChatList/ChatList.tsx
+++ front/src/components/ChatList/ChatList.tsx
@@ -1,29 +1,28 @@
 import { Container } from '@mantine/core';
 import { useEffect, useState } from 'react';
 import { getChatList } from '../../api/Chat.api';
-import { IChat } from '../../types/Chat';
 import Chat from './ChatItem/ChatItem';
 
 const ChatList = () => {
-  const [chatListElements, setChatListElements] = useState([] as any);
+  const [chatList, setChatList] = useState(new Array(20).fill({}));
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    renderChatList(new Array(20).fill({}), true);
     getChatList().then((res) => {
-      renderChatList(res.data as IChat[], false);
+      setChatList(res.data ?? []);
+      setLoading(false);
     });
   }, []);
 
-  const renderChatList = (chatList: IChat[], loading: boolean) => {
-    const renderedChatList = chatList?.map((chat, index) => (
+  const renderChatList = () => {
+    return chatList?.map((chat, index) => (
       <Chat key={index} chat={chat} loading={loading} />
     ));
-    setChatListElements([...renderedChatList]);
   };
 
   return (
     <Container size={'xs'} px={0} pt={20}>
-      {chatListElements}
+      {renderChatList()}
     </Container>
   );
 };
--- front/src/pages/Layout/Layout.tsx
+++ front/src/pages/Layout/Layout.tsx
@@ -1,13 +1,13 @@
 import { Container } from '@mantine/core';
 import BottomNavbar from '../../components/BottomNavbar/BottomNavbar';
 
-function Layout({ children }: { children: React.ReactNode }) {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <Container px={0} className="max-h-screen overflow-hidden">
       {children}
       <BottomNavbar isShadow />
     </Container>
   );
-}
+};
 
 export default Layout;
