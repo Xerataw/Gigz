7e5e76feb7536131dfdc2cc0e6048131e1b8c264
Oxy <lucas.nouhaud@gmail.com>
2023-07-25 14:03:37 +0200

    feat: begin to replace the values by envs

--- back/src/composables/useEmail.ts
+++ back/src/composables/useEmail.ts
@@ -1,5 +1,28 @@
-import { createTransport } from 'nodemailer';
+import nodemailer from 'nodemailer';
+import { MailOptions } from 'nodemailer/lib/sendmail-transport';
 
-const mailConfig = {
-  host: '',
+import useUtils from './useUtils';
+
+// const {} from useUtils();
+
+const config = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: true,
+  auth: {
+    user: 'ella71@ethereal.email',
+    pass: 'txQEE4sGctuNqcPeRv',
+  },
 };
+
+const transporter = nodemailer.createTransport(config);
+
+const sendMail = async (email: MailOptions) => {
+  await transporter.sendMail(email);
+};
+
+const useEmail = () => {
+  sendMail;
+};
+
+export default useEmail;
