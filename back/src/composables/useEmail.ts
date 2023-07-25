b1235e10993d3884ad7dddf6ec4a069547e5065d
Oxy <lucas.nouhaud@gmail.com>
2023-07-25 14:50:14 +0200

    feat: nodemailer is configured to work with an smtp server

--- back/src/composables/useEmail.ts
+++ back/src/composables/useEmail.ts
@@ -3,19 +3,17 @@ import { MailOptions } from 'nodemailer/lib/sendmail-transport';
 
 import useUtils from './useUtils';
 
-// const {} from useUtils();
+const { getEnv } = useUtils();
 
-const config = {
-  host: 'smtp.ethereal.email',
-  port: 587,
+const transporter = nodemailer.createTransport({
+  host: getEnv('NODEMAILER_HOST'),
+  port: Number(getEnv('NODEMAILER_PORT')),
   secure: true,
   auth: {
-    user: 'ella71@ethereal.email',
-    pass: 'txQEE4sGctuNqcPeRv',
+    user: getEnv('NODEMAILER_USER'),
+    pass: getEnv('NODEMAILER_PASS'),
   },
-};
-
-const transporter = nodemailer.createTransport(config);
+});
 
 const sendMail = async (email: MailOptions) => {
   await transporter.sendMail(email);
