e4277b8162fce65b0a5be10cc31c0a4902e2ee57
iittooss <brgtamaury@gmail.com>
2023-09-05 11:04:31 +0200

    feat(gallery): can add pictures

--- front/src/components/ProfileView/Gallery.tsx
+++ front/src/components/ProfileView/Gallery.tsx
@@ -1,8 +1,20 @@
 import { Carousel } from '@mantine/carousel';
-import { Center, Image, Loader } from '@mantine/core';
+import {
+  Button,
+  Card,
+  Center,
+  FileButton,
+  Image,
+  Loader,
+  Text,
+} from '@mantine/core';
+import { IconPlus } from '@tabler/icons-react';
+import { t } from 'i18next';
+import { useEffect, useState } from 'react';
 import GigzFetcher from '../../services/GigzFetcher';
 import EMediaType from '../../types/EMediaType';
 import IMedia from '../../types/IMedia';
+import { handleAddFiles, maxFile } from '../Steps/PresentationPicturesStep';
 
 interface IGalleryProps {
   mediaList: IMedia[];
@@ -16,6 +28,18 @@ const Gallery: React.FC<IGalleryProps> = ({
   withEmbed = false,
 }) => {
   const slidesHeight = withEmbed ? '35.75rem' : '42.25rem';
+  const isOnProfile = window.location.pathname.includes('profile');
+  const [pictures, setPictures] = useState<IMedia[]>(
+    mediaList.filter((media) => media.type === EMediaType.IMAGE)
+  );
+
+  const handleSetPictures = (pictures: IMedia[]) => {
+    setPictures((old) => [...pictures, ...old]);
+  };
+
+  useEffect(() => {
+    console.log('update pictures :', pictures);
+  }, [pictures]);
 
   return loading ? (
     <Center h={slidesHeight}>
@@ -23,17 +47,45 @@ const Gallery: React.FC<IGalleryProps> = ({
     </Center>
   ) : (
     <Carousel withIndicators withControls={false} height={slidesHeight}>
-      {mediaList.map(
-        (media) =>
-          media.type === EMediaType.IMAGE && (
-            <Carousel.Slide key={media.id}>
-              <Image
-                src={GigzFetcher.getImageUri(media.media)}
-                height={slidesHeight}
-                alt="Gallery"
-              />
-            </Carousel.Slide>
-          )
+      {pictures.map((media) => (
+        <Carousel.Slide key={media.id}>
+          <Image
+            src={GigzFetcher.getImageUri(media.media)}
+            height={slidesHeight}
+            alt="Gallery"
+          />
+        </Carousel.Slide>
+      ))}
+      {isOnProfile && (
+        <Carousel.Slide key="edit profile">
+          <Card
+            shadow="sm"
+            radius="md"
+            withBorder
+            className="h-[200px] py-[5px] flex flex-col justify-center"
+          >
+            <div>
+              <Text align="center" mb="sm" weight={500}>
+                {t('register.presentationPicturesStep.add')}
+              </Text>
+              <Center>
+                <FileButton
+                  onChange={(e) =>
+                    handleAddFiles(e, handleSetPictures, pictures, maxFile)
+                  }
+                  accept="image/png,image/jpeg"
+                  multiple
+                >
+                  {(props) => (
+                    <Button {...props}>
+                      <IconPlus />
+                    </Button>
+                  )}
+                </FileButton>
+              </Center>
+            </div>
+          </Card>
+        </Carousel.Slide>
       )}
     </Carousel>
   );
